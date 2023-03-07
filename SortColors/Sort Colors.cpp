2c56433b26255372ce106ae92236d5ed32d42ae6
ritik21311 <65362957+ritik21311@users.noreply.github.com>
2023-03-07 23:37:57 +0530

    Add files via upload

---
+++ SortColors/Sort Colors.cpp
@@ -0,0 +1,23 @@
+
+// Function to sort colors
+void sortColors(vector<int>& nums) {
+        int l=0;
+        int m=0;
+        int h=nums.size()-1;
+        while(m<=h){
+            if(nums[m]==0){
+                swap(nums[l],nums[m]);
+                l++;
+                m++;
+            }
+            else if(nums[m]==1){
+                m++;
+            }
+            else{
+                swap(nums[m],nums[h]);
+                h--;
+            }
+             
+        }
+        
+    }
